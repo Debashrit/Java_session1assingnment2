a228ce521f9db4532d9983df6393418ccf9350d7
Debashrit <debashritmohanty.mech@gmail.com>
2016-10-20 17:53:04 +0530

    JAVA session1

---
+++ assignment13.java
@@ -0,0 +1,12 @@
+
+public class assignment13 {
+	public static void main(String[] args){
+		int x=6,y=8;
+		x=x+y;
+		y=x-y;
+		x=x-y;
+		System.out.println(x);
+		System.out.println(y);
+	}
+
+}
---
+++ assignment14.java
@@ -0,0 +1,8 @@
+
+public class assignment14 {
+public static void main(String[] args){
+	for(int i=65;i<=90;i++){
+		System.out.println((char)i+"-"+i+" ");
+	}		
+}
+}
