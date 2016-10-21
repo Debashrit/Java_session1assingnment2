63abd3edd4a74d3f291676fc5c8dfabe4517e7c9
Debashrit <debashritmohanty.mech@gmail.com>
2016-10-21 19:34:29 +0530

    assignment11

---
+++ assignment11.java
@@ -0,0 +1,13 @@
+
+public class assignment11 {
+
+	public static void main(String[] args)
+    {
+        int a=12,b=23;
+        int sum;
+        sum=a-(-b);
+        System.out.println(sum);
+        
+    }
+}
+
