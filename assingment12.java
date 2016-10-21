5785410ce321e489f1f4921b04e51ad67bdcf9fb
Debashrit <debashritmohanty.mech@gmail.com>
2016-10-21 08:32:06 +0530

    session1assin2

---
+++ assingment12.java
@@ -0,0 +1,16 @@
+
+public class assingment12 {
+	public static void main(String[] args){
+		int a=10,b=20;
+		int B= a-- - --a;
+		int c= a--;
+		int d=a>>2;
+		int e=a&b;
+		System.out.println(B);
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println(e);
+		
+	}
+
+}
